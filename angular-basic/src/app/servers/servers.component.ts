303beedce4cfb849e2c087bbd7f4fe593f9c8365
Rafael Silva <rafaelsilvarnj@gmail.conm>
2020-05-07 18:56:20 -0300

    Basics
    Basics of angular on a simple project (components, databinding, property binding, directives)

---
+++ angular-basic/src/app/servers/servers.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server created';
+  serverName: string = '';
+  serverCreated: boolean = false;
+  servers: Array<string> = ['Test server', 'Test server 2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreated=true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was created with the name ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+}

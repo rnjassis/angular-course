70968eb2932ff3a3ca79e99314d9ddb4b03fd9c0
Rafael Silva <rafaelsilvarnj@gmail.conm>
2020-05-14 15:01:54 -0300

    Changed the manipulation of accounts and logging to a server

--- angular-services/src/app/app.component.ts
+++ angular-services/src/app/app.component.ts
@@ -1,25 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account-service/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
+  accounts: {name: string, status: string}[]=[];
+
+  constructor(private accountService: AccountService){
+
+  }
+
+  ngOnInit(){
+    this.accounts = this.accountService.accounts;
+  }
 
   onAccountAdded(newAccount: {name: string, status: string}) {
     this.accounts.push(newAccount);

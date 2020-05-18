b972c07da0912b47b95acb5a1d2dab2f4dfa2d39
Rafael Silva <rafaelsilvarnj@gmail.conm>
2020-05-17 23:50:07 -0300

    Added routing to recipes project

--- recipes/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipes/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe-service/recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,13 +8,8 @@ import { RecipeService } from '../../recipe-service/recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
+  @Input() index: number;
 
   ngOnInit(): void {
-  }
-
-  onSelected(){
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 }
--- recipes/src/app/recipes/recipe-service/recipe.service.ts
+++ recipes/src/app/recipes/recipe-service/recipe.service.ts
@@ -6,7 +6,8 @@ import { ShoppingListService } from 'src/app/shopping-list/shopping-list-service
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Test recipe','Description of a test','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg', [new Ingredient('meat',1), new Ingredient('flour', 200)])
+    new Recipe('Test recipe','Description of a test','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg', [new Ingredient('meat',1), new Ingredient('flour', 200)]),
+    new Recipe('Test recipe 2','Description of a test but now is 2','https://media.gazetadopovo.com.br/2019/11/20171835/HAMBURGUERIA-AGUA-VERDE_PE%CC%81-VERMEIO_RAFAEL_FESTIVAL-BURGER-FUNFEST_ESTU%CC%81DIO-A%CC%80-MILANESA-960x540.jpg', [new Ingredient('meat',1), new Ingredient('buns', 2), new Ingredient('bacon', 5)])
   ];
 
   constructor(private shoppingListService: ShoppingListService){
@@ -17,6 +18,10 @@ export class RecipeService {
 
   getRecipes(){
     return this.recipes.slice();
+  }
+
+  getRecipe(id: number){
+    return this.recipes[id];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){

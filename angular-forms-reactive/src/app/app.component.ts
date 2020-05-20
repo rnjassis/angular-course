900f05b0868cf971b7957ba45f397ba79e963cc3
Rafael Silva <rafaelsilvarnj@gmail.conm>
2020-05-20 20:32:09 -0300

    Added reactive form
    - Created reactive form
    - Added validators
    -- Added asynchronous validators

--- angular-forms-reactive/src/app/app.component.ts
+++ angular-forms-reactive/src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbiddenUserNames=['Chris', 'Anna'];
+
+  ngOnInit(){
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username':new FormControl(null, [Validators.required, this.forbiddenNamesValidator.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmailsValidator),
+    }),
+      'gender': new FormControl('male'),
+      'hobbies':new FormArray([])
+    });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    // this.signupForm.statusChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+  }
+
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+
+  onAddHobby(){
+  const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
+  forbiddenNamesValidator(control: FormControl): {[s:string]:boolean}{
+    if(this.forbiddenUserNames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden':true}
+    }
+    return null;
+  }
+
+  forbiddenEmailsValidator(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsForbidden': true});
+        }else{
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
 }
